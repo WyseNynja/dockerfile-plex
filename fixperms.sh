2af327d06ece57d59a522b3ab76d977909e3929e
Bryan Stitt <bryan@stitthappens.com>
2017-08-08 10:33:44 -0700

    use plex, not abc user

--- fixperms.sh
+++ fixperms.sh
@@ -4,8 +4,8 @@ echo "Fixing perms..."
 
 for x in /config "/config/Library/Application Support/Plex Media Server/Logs" /transcode /data/*
 do
-    if [ "$(stat -c '%U' "$x")" != "abc" ]; then
-        chown -R abc:abc "$x"
+    if [ "$(stat -c '%U' "$x")" != "plex" ]; then
+        chown -R plex:plex "$x"
         echo " - $x"
     fi
 done
