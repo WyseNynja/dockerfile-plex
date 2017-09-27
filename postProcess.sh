e548829dc7aefaad062cd2da776f505cff0e8b68
Bryan Stitt <bryan@stitthappens.com>
2017-09-26 17:05:15 -0700

    allow ccextractor to fail

--- postProcess.sh
+++ postProcess.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -e
+set -eu
 
 # skip commercials
 python3 /opt/PlexComskip/PlexComskip.py "$1"
 
 echo "Extracting subtitles for $1..."
-ccextractor -srt -12 "$1"
+ccextractor -srt -12 "$1" || echo No subtitles
 # TODO: "--service all" ?
 
 # if you wanted, you could remux here, but plex supports .srt files
